7e846997bdeddc5db4d0259acb98f9c780824f62
Mike Silvis <mikesilvis@gmail.com>
2015-01-06 09:27:44 -0800

    adjusting the heading size

--- sportsball-ios/Screens/ScoreShow/Views/ScoreDetail/ScoreDetailCollectionViewCell.m
+++ sportsball-ios/Screens/ScoreShow/Views/ScoreDetail/ScoreDetailCollectionViewCell.m
@@ -14,7 +14,7 @@
 @implementation ScoreDetailCollectionViewCell
 
 static int const cellRowHeight = 40;
-static int const cellRowHeaderHeight = 20;
+static int const cellRowHeaderHeight = 25;
 static NSString * const scoreDetailInfoCell = @"scoreDetailInfoCollectionViewCell";
 static NSString * const scoreDetailHeaderCell = @"scoreDetailHeaderCollectionViewCell";
 
